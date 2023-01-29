fd9fbea80962b99562bf2d1f6a90cbbbbd2caa1a
backas36 <backas36@gmail.com>
2023-01-30 00:56:12 +0800

    update

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ export default App;
  * axios (A) V.S react query with axios or rtk query (B)
  * 1. fetch data before component mounted when using B
  * 2. less code, and no need to handle loading, error states for fetching when using B
+ * 3. it's no need to fetch again since cached data when using B
  *
  *
  *
  * rtk query V.S rtk query
- *
+ * 1. rtk query integrated with react toolkit better than react query
+ * 2. rtk query managed api in one place
  */
